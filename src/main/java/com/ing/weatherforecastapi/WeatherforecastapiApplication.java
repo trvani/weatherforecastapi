940b8e23105a779b4050567e4680534ae0cdcf87
trvani <47416856+trvani@users.noreply.github.com>
2023-11-03 22:18:07 +0300

    Initial commit

---
+++ src/main/java/com/ing/weatherforecastapi/WeatherforecastapiApplication.java
@@ -0,0 +1,13 @@
+package com.ing.weatherforecastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherforecastapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherforecastapiApplication.class, args);
+	}
+
+}
